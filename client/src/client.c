462d5d20a9da73f8bf7ebdd7c1b31e902cea622d
juanandreacchio <juan.andreacchio@gmail.com>
2024-04-01 23:05:40 +0000

    solucion

--- client/src/client.c
+++ client/src/client.c
@@ -16,6 +16,8 @@ int main(void)
 
 	logger = iniciar_logger();
 
+	log_info(logger,"Hola, soy un log");
+
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
@@ -23,6 +25,11 @@ int main(void)
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
 	config = iniciar_config();
+
+	valor = string_duplicate(config_get_string_value(config, "CLAVE"));
+
+	log_info(logger,valor);
+
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
@@ -43,6 +50,8 @@ int main(void)
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
 
+	enviar_mensaje(valor,conexion);
+
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
@@ -56,12 +65,16 @@ t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
 
+	nuevo_logger = log_create("tp0.log", "tp0", true ,  LOG_LEVEL_INFO);
+
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
+
+	nuevo_config = config_create("cliente.config");
 
 	return nuevo_config;
 }
@@ -74,7 +87,14 @@ void leer_consola(t_log* logger)
 	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+	while (strcmp(leido,""))
+	{
+		log_info(logger,leido);
+		free(leido);
+		leido = readline("> ");
+	};
 
+	free(leido);
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
 
@@ -86,15 +106,30 @@ void paquete(int conexion)
 	char* leido;
 	t_paquete* paquete;
 
+	leido = readline("> ");
+
 	// Leemos y esta vez agregamos las lineas al paquete
+	while (strcmp(leido,""))
+	{
+		agregar_a_paquete(paquete,leido);
+		free(leido);
+		leido = readline("> ");
+	}
+	enviar_paquete(paquete,conexion);
 
+	free(leido);
 
+	eliminar_paquete(paquete);
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
 	
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
+
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 }
